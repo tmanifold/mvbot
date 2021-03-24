bfbb372de7ec1b50b32aee10a74262cb3461edda
tmanifold <tdmanifold@gmail.com>
2021-03-24 02:26:31 -0400

    rewrote a bunch of code. added switched and bulk move. need to get command input validated.

---
+++ changelog.md
@@ -0,0 +1,25 @@
+# Changelog
+
+## [1.0.0] -
+### Added
+- Bulk Move
+### Changed
+- Command structure is now switch based.
+- Rewrote sections of code and generally cleaned the place up a bit.
+
+## [0.1.2] - 2020-11-15
+### Fixed
+- A bug that caused attached images to not be moved correctly
+
+## [0.1.1] - 2020-11-02
+### Changed
+- The invoking command message is now deleted.
+
+## [0.1.0] - 2020-05-17
+### Added
+- Ability to reference message by its link
+- `@mention` original poster, mover, and link original `#channel`
+### Changed
+- Bot messages are now plaintext with blockquotes instead of embeds.
+### Fixed
+- A bug that caused old posts from mvbot to not be deleted when moving to a new location
--- mvbot.js
+++ mvbot.js
@@ -4,9 +4,284 @@ const pkg_info = require('./package.json')
 
 var bot = new Client();
 
+/*
+    usage: Display help message and useage information
+    parameters:
+        channel (Channel): The channel to which the help message should be written
+    return: None
+*/
 function usage(channel) {
 
-    channel.send('usage:\n `!mv <message-id> <target channel> ["reason"]`');
+    channel.send('usage:\n `!mv <message-id> <target channel> ["reason"]`').catch(console.log);
+}
+
+/*
+    errorMessage: Send an error message
+    parameters:
+        channel (Channel): The channel to which the error message should be written
+        message (String): The error message to send in the channel
+    return: None
+*/
+function errorMessage(channel, message) {
+
+        channel.send(message);
+        usage(channel);
+}
+
+/*
+    parseOption: Parse an option from the given command. Returns substring of args
+                from startidx to indexOf(endchar)
+    parameters:
+        args (String): argument string to parse
+        startidx (int): staring index to begin parsing
+        enchar  (char): character to parse until.
+    return: String containing the parsed option value
+*/
+function parseOption(args, startidx, endchar) {
+
+    let endidx = args.indexOf(endchar, startidx);
+
+    return args.substring(startidx, (endidx == -1 ? args.length : endidx));
+}
+
+/*
+    getopts: Extracts options from the given command
+    parameters:
+        cmd (Message): The command from which to extract arguments
+    return: A Map of the options and their values
+*/
+function getopts (cmd) {
+
+    // remove the !mv invocation from the command string
+    var args = cmd.content.slice(4);
+
+    //console.log(args);
+
+    var message = ''; // target message
+    var destination = ''; // destination chanenel
+    var comment = ''; // move comment
+    var numMessages = 1;  // number of messges to move
+
+    // get the indexOf to find start indices of options
+    var m = args.indexOf('-m');
+    var d = args.indexOf('-d');
+    var c = args.indexOf('-c');
+    var n = args.indexOf('-n');
+    var N = args.indexOf('-N');
+
+    // check if the target message and destination are specified
+    if (m == -1 || d == -1) {
+
+        cmd.channel.send('Invalid arguments: message and destination must be specified.').catch(console.error);
+        usage(cmd.channel);
+        //errorMessage(cmd.channel, 'Invalid arguments: message and destination must be specified.');
+        return;
+    }
+
+    // add 3 characters to offset for switch width and whitespace
+    m += 3;
+    d += 3;
+
+    // Map to match command switches to argument values
+    var mapOptions = new Map();
+
+    // use the option indices to parse message and destination
+    message = parseOption(args, m, ' ');
+    destination = parseOption(args, d, ' ').replace(/<#/, '').replace(/>/, '');
+
+    // if the message is given by URL, extract its ID
+    if (message.search(/^(https:\/\/discord.com\/channels\/)/) >= 0) {
+        // The target is given by url
+        message = message.substring(message.lastIndexOf('/') + 1);
+    }
+
+    // add them to the map
+    mapOptions.set('m', message);
+    mapOptions.set('d', destination);
+
+    // process the comment option
+    if (c != -1) {
+
+        // check for the opening quotation
+        commentStart = args.indexOf('"', c + 3);
+        commentEnd = args.indexOf('"', commentStart + 1);
+
+        if (commentStart == -1 || commentEnd == -1) {
+            errorMessage(cmd.channel, 'Invalid arguments: comments must be enclosed in double quotes. Example: `\"comment\"`');
+            return;
+        }
+
+        comment = args.substring(commentStart + 1, commentEnd);
+    }
+    mapOptions.set('c', comment);
+
+    // set repeating header option
+    mapOptions.set('repeatHeader', true);
+
+    // process the numMessages option
+    if (n != -1 && N != -1) {
+
+        errorMessage(cmd.channel, 'Invalid arguments: `-n` and `-N` are mutually exclusive.')
+
+    } else if (n != -1) { // set number of subsequent messages to move
+
+        numMessages = parseInt(parseOption(args, n + 3, ' '));
+
+    } else  if (N != -1) { // update repeating header option
+        numMessages = parseInt(parseOption(args, N + 3, ' '));
+        mapOptions.set('repeatHeader', false);
+    }
+
+    // return if numMessages is NaN
+    if (typeof numMessages != "number") {
+
+        errorMessage(cmd.channel, 'Invalid arguments: value for `-n` must be an integer.');
+        return;
+    }
+
+    mapOptions.set('n', numMessages);
+    console.log(mapOptions);
+
+    return mapOptions;
+}
+
+/*
+    moveMessage: Copy a specified message into the target channel
+    parameters:
+        msg (Message): Reference to the message object we are moving
+        targetChannel (Channel): The channel to which the message should be moved
+    return: None
+*/
+function moveMessage (msg, targetChannel) {
+
+    // build out the message to send to the target channel
+    var embeds = [];
+    var attachments = [];
+
+    // gather original message embeds
+    if (msg.embeds.length > 0) {
+
+        msg.embeds.forEach( element => {
+
+            //console.log(element);
+            embeds.push(element);
+
+        }, err => {
+            console.log(err);
+        });
+
+    } else {
+
+        // gather original message attachments
+        if (msg.attachments.size > 0) {
+
+            msg.attachments.each(a => {
+                //console.log(a);
+                attachments.push(a.proxyURL);
+            });
+        }
+    }
+
+    // send the original message content as a new message to the targetChannel
+    targetChannel.send(msg.content == '' ? '' : '>>> ' + msg.content, {
+        files: attachments,
+        embeds: embeds
+    });
+
+   msg.delete(); // delete message original message
+}
+
+/*
+    mvbotHeader: Generates the mvbot message header
+    parameters:
+        msg (Message): The message object to be moved
+        mover (Member): The user invoking the command
+        comment (str): option comment
+    return: The completed header string
+*/
+function mvbotHeader (msg, mover, comment = '') {
+
+    // build out the message to send to the target channel
+    // FORMAT:
+    // @author | #original-channel
+    // Weekday Month Day Year HH:MM:SS
+    // "REASON" - @mover
+
+    var h = '<@' + /*(msg.member ? msg.member : msg.author)*/ msg.author + '> in <#' + msg.channel + '>\n';
+    h += msg.createdAt + '\n';
+    h += (comment == '') ? '' : '*\"' + comment + '\"*';
+    h += ' - <@' + mover + '>\n\n';
+
+    return h;
+}
+
+/*
+    processCommand: Begin processing commands sent to the bot
+    parameters:
+        cmd:    The command sent to the bot
+    return: none
+*/
+function processCommand (cmd) {
+
+    // extract options from the command string
+    var args = getopts(cmd);
+
+    // verify permissions of the user invoking the command
+    if (!cmd.member.hasPermission('MANAGE_MESSAGES')) {
+
+        cmd.channel.send('Sorry, ' + cmd.member.displayName + '. I can\'t let you do that.');
+        return;
+    }
+
+    // get a reference to the target channel
+    var targetChannel = bot.channels.resolve(args.get('d'));
+
+    // ensure it exists
+    if (!targetChannel) {
+        errorMessage(cmd.channel, 'The target channel doesn\'t exist!');
+        return;
+    }
+
+    // set batch move options
+    var repeat = args.get('repeatHeader');
+
+    // get a reference to the MessagesManager for the current channel
+    var currentChannelMessages = cmd.channel.messages;
+
+    // Fetch the desired message
+    currentChannelMessages.fetch(args.get('m')).then( msg => {
+
+        // move the initial message
+        targetChannel.send(mvbotHeader(msg, cmd.member, args.get('c')));
+        moveMessage(msg, targetChannel);
+
+        // move subsequent messages
+        if (args.get('n') > 1) {
+
+            // fetch n messages after the original specified message
+            currentChannelMessages.fetch({limit: args.get('n') - 1, after: args.get('m')}).then( followingMessages => {
+
+                // move each message
+                followingMessages.sort().each( m => {
+                    if (repeat) {
+                        targetChannel.send(mvbotHeader(msg, cmd.member, args.get('c')));
+                    }
+                    moveMessage(m, targetChannel);
+                });
+
+            }, err => {
+                console.log(err);
+            });
+        }
+
+        //cmd.delete(); // delete invoking command
+
+   }, e => {
+       cmd.channel.send('I was unable to find the message you want to move. Ensure you are entering a valid message ID.');
+       console.log(e);
+   });
+
+
 }
 
 bot.on('ready', () => {
@@ -23,147 +298,10 @@ bot.on('ready', () => {
 
 bot.on('message', message => {
 
-
+    // check message for bot invocation
     if (message.content.startsWith('!mv')) {
 
-        args = message.content.split(' ');
-
-        // syntax: !mv <message> <channel> "<reason>"
-
-        if (!args[2]) {
-            message.channel.send('Invalid number of arguments');
-            usage(message.channel);
-            return;
-        }
-
-        var target_message = '';
-
-        if (args[1].search(/^(https:\/\/discordapp.com\/channels\/)/) >= 0) {
-            // The target is given by url
-            target_message = args[1].substring(args[1].lastIndexOf('/') + 1);
-        } else {
-            // assume a message id is given
-            target_message = args[1];
-        }
-
-        message.channel.messages.fetch(target_message).then(msg => {
-
-            if (!message.member.hasPermission("MANAGE_MESSAGES")) {
-                
-                message.channel.send('Sorry, ' + message.member.displayName + '. I can\'t let you do that.');
-                return;
-            }
-
-                        
-            target_channel = bot.channels.resolve(
-                args[2].replace(/<#/, '')
-                        .replace(/>/, '')
-                        );
-
-            if (!target_channel) {
-                message.channel.send('The target channel doesn\'t exist!');
-                usage(message.channel);
-                return;
-            }
-
-            var bot_embed = new MessageEmbed();
-
-            // determine if the msg was posted by the bot itself
-            // if (msg.author.tag == bot.user.tag) {
-
-            //     if (msg.embeds.length > 0) {
-            //         embed = new MessageEmbed(msg.embeds[0]);
-
-            //         msg.attachments.forEach( element => {
-            //             embed.attachFiles(element);
-            //         });
-
-            //         target_channel.send(embed);
-            //         msg.delete();
-
-            //         return;
-            //     }
-            // }
-
-            var mvstr = '<@' + msg.member + '> | <#' + message.channel + '>\n';
-            mvstr += message.createdAt + '\n';
-            mvstr += (args[3] == null) ? '' : '*\"' + message.content.split('\"', 2)[1] + '\"*';
-            mvstr += ' - <@' + message.member + '>\n\n';
-            var embeds = [];
-            var attachments = [];
-
-            // bot_embed.setAuthor(message.author.username, message.author.avatarURL())
-            // //.setTitle('#' + message.channel.name)
-            // .setDescription(message.createdAt)
-            // .setFooter('Moved by @' + message.member.displayName + ' with mvbot.' 
-            //     + (args[3] == null ? '' : ' Reason: ' + message.content.split('\"', 2)[1]))
-            // .setTimestamp(new Date());
-            
-            var urls = '';
-
-            if (msg.embeds.length > 0) {
-                
-                //console.log(msg.embeds);
-
-                // loop through embeds and process them base on their .type
-
-                msg.embeds.forEach( element => {
-
-                    console.log(element);
-                    embeds.push(element);
-                  // urls += element.url + '\n';
-
-                }, err => {
-                    console.log(err);
-                });    
-                
-                // embeds.push(bot_embed);
-
-            } else {
-
-                if (msg.attachments.size > 0) {
-
-                    msg.attachments.each(a => {
-                        console.log(a);
-                        attachments.push(a.url);
-
-                        //urls += a.url + '\n';
-                    });
-                    //embed.setImage(msg.attachments.first().attachment);
-                }  
-            }
-
-            //console.log(args);
-
-
-                //embed.addField(' ', message.content);
-            // var urls = '';
-
-            // msg.attachments.forEach( a => {
-
-            //     urls += a.url + '\n';
-            // });
-
-      
-
-                //target_channel.send(embed);
-
-            
-            
-            target_channel.send(msg.content == '' ? mvstr :  mvstr + '>>> ' + msg.content, {
-                files: attachments,
-                embeds: embeds
-            });
-
-            // if (msg.content) {
-            //     target_channel.send(msg.content);
-            // }
-            // target_channel.send(embed);
-
-           msg.delete();
-        }, () => {
-            message.channel.send('I was unable to find the message you want to move. Ensure you are entering a valid message ID.');
-        });
+        processCommand(message);
     }
 
 });
