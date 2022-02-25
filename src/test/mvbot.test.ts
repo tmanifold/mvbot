8941a4a2d4a4b243b7cad65192f750b48ec0801e
tmanifold <tdmanifold@gmail.com>
2022-02-25 16:13:47 -0500

    test: add message and command tests for mvbot. add mvbotCommand test file

--- src/test/mvbot.test.ts
+++ src/test/mvbot.test.ts
@@ -1,3 +1,4 @@
+
 import * as Discord from 'discord.js';
 // import * as Mvbot from '../mvbot';
 import { MvbotChannel } from '../mvbot';
@@ -13,13 +14,12 @@ import { MvbotChannel } from '../mvbot';
     const bot = new Mvbot.Mvbot();
     // bot.start(Mvbot.Auth.token.dev);
     
-    let guild: Discord.Guild;
-    let channel: Discord.GuildTextBasedChannel;
+    var guild: Discord.Guild;
+    var channel: Discord.GuildTextBasedChannel;
     // const channel: Discord.GuildTextBasedChannel = await guild.channels.fetch(test_cfg.channel) as Discord.GuildTextBasedChannel;
 
     beforeAll(() => {
         jest.setTimeout(10000);
-        
     });
 
     afterAll(() => {
@@ -33,19 +33,52 @@ import { MvbotChannel } from '../mvbot';
             return bot.start(Mvbot.Auth.token.dev);
         });
 
-        test('Ready check', () => {
-            // expect.assertions(1);
-            //return expect(bot.start(Mvbot.Auth.token.dev)).resolves.toBe(Mvbot.Auth.token.dev);
-            expect(bot.isReady()).toBe(true);
+        test('Guild reference', () => {
+            return bot.guilds
+                .fetch(test_cfg.guild)
+                    .then(g => {
+                        guild = g;
+                    })
+                    .catch(console.error);
         });
+
+        test('Channel reference', () => {
+            return guild.channels
+                .fetch(test_cfg.channel)
+                    .then(ch => {
+                        channel = ch as Discord.GuildTextBasedChannel;
+                    })
+                    .catch(console.error);
+        });
+
     });
-
-
     
+    var test_string: string = '!mv test_target' + Date.now();
+    var message: Discord.Message;
     
-    // test('Send message', () => {
-    //     channel.send('!mv test');
-    // });
+    describe('Messaging', () => {
 
+        test('Send message', () => {
+            return channel.send(test_string).then(m => {
+                message = m;
+            });
+        });
+
+        test('Read message content', () => {
+
+            expect(message.content).toEqual(test_string);
+        });
+
+    });
+    
+    describe('Commands', () => {
+
+        test('Tokenize command string', () => {
+
+            expect(bot.tokenizeCommand(message.content)).toEqual(bot.tokenizeCommand(test_string));
+        });
+
+
+    });
 
 })();
---
+++ src/test/mvbotCommand.test.ts
@@ -0,0 +1,14 @@
+
+import * as Discord from 'discord.js';
+const Mvbot = require('../mvbot');
+
+(async () => {
+
+    const cmd_str = '!mv -m 946858024151175188 -d <#706930498815131803>';
+    const command = new Mvbot.MvbotCommand(cmd_str);
+
+    test.skip('foo', () => {
+
+    });
+
+})();
